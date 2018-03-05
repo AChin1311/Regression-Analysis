186fd6d6d9bd5a9ee0085440b75758c78f252b5a
Linus Cheng <linuschenglight@gmail.com>
2018-03-05 02:01:34 -0800

    Ready

--- Q2-d-ii.py
+++ Q2-d-ii.py
@@ -47,9 +47,6 @@ for i in np.arange(5):
 
 
 
-#X_org = content_all_np[: , 0:5]
-#Y_org = content_all_np[: ,  5 ]
-
 
 del WorkFlow_list,File_list,cont_np, num_WorkFlow,num_File,i,X_i,Y_i,cont_list_i
 # delete unused variables
@@ -83,31 +80,6 @@ def kfold_ridg(X,Y,alpha):
 
     return avg_test_RMSE,avg_train_RMSE
 
-
-
-def kfold_lass(X,Y,alpha):
-    test_rmse  = []
-    train_rmse = []
-    kf = KFold(n_splits=10, random_state=None, shuffle=False)
-    fold_i =0
-    for train_id, test_id in kf.split(X):
-        fold_i += 1
-        X_train, X_test = X[train_id], X[test_id]
-        Y_train, Y_test = Y[train_id], Y[test_id]
-
-        model_1 = linear_model.Lasso(alpha)
-        model_1.fit(X_train,Y_train)
-
-        Y_test_pred  = model_1.predict(X_test)
-        Y_train_pred = model_1.predict(X_train)
-
-        test_rmse.append ( np.sqrt( metrics.mean_squared_error(Y_test , Y_test_pred )))
-        train_rmse.append( np.sqrt( metrics.mean_squared_error(Y_train, Y_train_pred)))
-
-    avg_test_RMSE   = sum(test_rmse)/10
-    avg_train_RMSE  = sum(train_rmse)/10
-
-    return avg_test_RMSE,avg_train_RMSE
 
 
 l1_ratio =0
@@ -156,6 +128,52 @@ for wf in np.arange(5):
     plt.xlabel("Order")
     plt.ylabel("RMSE")
     
+
+
+
+print("\n\n")
+""" ====== plotting predicted vs true value ====== """
+print("====== plot pred vs true output ======")
+
+
+
+alpha =0
+order_all = np.array([7,9,6,5,6])
+    
+for wf in np.arange(5):
+
+    
+    poly   = PolynomialFeatures( order_all[wf] )
+    X_poly = poly.fit_transform(  X_list[wf]   )
+    model_1 = linear_model.Ridge(alpha)
+    model_1.fit(X_poly,Y_list[wf])
+    Y_pred_1  = model_1.predict(X_poly)
+
+    plt.figure()
+    yy = Y_pred_1
+    
+    title_string = 'work_flow_%d'%wf + ', order = %d'%order_all[wf]
+    
+    plt.title(title_string)
+    xx = Y_list[wf]
+    plt.scatter(xx, yy, s = 1,  alpha=0.01)
+    plt.xlabel('true value')
+    plt.ylabel('fitted value')
+    xi = range(0,2)
+    yi = [i for i in xi]
+    plt.axis([-0.03,1.03,-0.03,1.03])
+    plt.plot(xi,yi,color='red')
+    
+    plt.figure()
+    xx = Y_pred_1
+    yy = np.abs(xx-Y_list[wf])
+    plt.scatter(xx, yy, s=1, alpha=0.01)
+    plt.axis([-0.03,1.03,-0.03,1.03])
+    plt.xlabel('fitted value')
+    plt.ylabel('residuals')
+
+
+
 
 print("completed")
 
