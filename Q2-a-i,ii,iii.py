574284d22a5669851b760b0a2a63db48dc3da81e
Linus Cheng <linuschenglight@gmail.com>
2018-03-05 01:58:22 -0800

    Ready

--- Q2-a-i,ii,iii.py
+++ Q2-a-i,ii,iii.py
@@ -6,7 +6,7 @@ from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
 #from sklearn import preprocessing
 from sklearn.feature_selection import f_regression, mutual_info_regression
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 
@@ -99,8 +99,6 @@ test_rmse_q2,train_rmse_q2,all_test_RMSE_q2 = kfold_lin_reg(scaler.fit_transform
 
 
 
-
-
 print("\n\n")
 """ ====== <iii> Feature Selection ====== """
 print("====== <iii> Feature Selection ======")
@@ -129,5 +127,126 @@ test_rmse_q3_mi,train_rmse_q3_mi,all_test_RMSE_q3_mi = kfold_lin_reg(X[:,2:5],Y)
 
 
 
+
+print("\n\n")
+""" ====== plotting predicted vs true value ====== """
+print("====== plot pred vs true output ======")
+
+
+model_1 = linear_model.LinearRegression()
+model_1.fit(X,Y)
+Y_pred_1  = model_1.predict(X)
+
+scaler = StandardScaler()
+model_2 = linear_model.LinearRegression()
+model_2.fit(scaler.fit_transform(X),Y)
+Y_pred_2  = model_2.predict(scaler.fit_transform(X))
+
+model_3 = linear_model.LinearRegression()
+model_3.fit(X[:,1:4],Y)
+Y_pred_3  = model_3.predict(X[:,1:4])
+
+model_4 = linear_model.LinearRegression()
+model_4.fit(X[:,2:5],Y)
+Y_pred_4  = model_4.predict(X[:,2:5])
+
+
+
+print("===1===")
+
+plt.figure()
+yy = Y_pred_1
+plt.title("i linear regression")
+xx = Y
+plt.scatter(xx, yy, s = 1,  alpha=0.01)
+plt.xlabel('true value')
+plt.ylabel('fitted value')
+xi = range(0,2)
+yi = [i for i in xi]
+plt.axis([-0.03,1.03,-0.03,1.03])
+plt.plot(xi,yi,color='red')
+
+plt.figure()
+xx = Y_pred_1
+yy = np.abs(xx-Y)
+plt.scatter(xx, yy, s=1, alpha=0.01)
+plt.axis([-0.03,1.03,-0.03,1.03])
+plt.xlabel('fitted value')
+plt.ylabel('residuals')
+
+##########
+print("===2===")
+
+plt.figure()
+yy = Y_pred_2
+plt.title("ii standardized linear regression")
+xx = Y
+plt.scatter(xx, yy, s = 1,  alpha=0.01)
+plt.xlabel('true value')
+plt.ylabel('fitted value')
+xi = range(0,2)
+yi = [i for i in xi]
+plt.axis([-0.03,1.03,-0.03,1.03])
+plt.plot(xi,yi,color='red')
+
+plt.figure()
+xx = Y_pred_2
+yy = np.abs(xx-Y)
+plt.scatter(xx, yy, s=1, alpha=0.01)
+plt.axis([-0.03,1.03,-0.03,1.03])
+plt.xlabel('fitted value')
+plt.ylabel('residuals')
+
+##########
+print("===3===")
+
+plt.figure()
+yy = Y_pred_3
+plt.title("iii f_regression")
+xx = Y
+plt.scatter(xx, yy, s = 1,  alpha=0.01)
+plt.xlabel('true value')
+plt.ylabel('fitted value')
+xi = range(0,2)
+yi = [i for i in xi]
+plt.axis([-0.03,1.03,-0.03,1.03])
+plt.plot(xi,yi,color='red')
+
+plt.figure()
+xx = Y_pred_3
+yy = np.abs(xx-Y)
+plt.scatter(xx, yy, s=1, alpha=0.01)
+plt.axis([-0.03,1.03,-0.03,1.03])
+plt.xlabel('fitted value')
+plt.ylabel('residuals')
+
+
+##########
+print("===4===")
+
+plt.figure()
+yy = Y_pred_4
+plt.title("iii mutual_info_regression")
+xx = Y
+plt.scatter(xx, yy, s = 1,  alpha=0.01)
+plt.xlabel('true value')
+plt.ylabel('fitted value')
+xi = range(0,2)
+yi = [i for i in xi]
+plt.axis([-0.03,1.03,-0.03,1.03])
+plt.plot(xi,yi,color='red')
+
+plt.figure()
+xx = Y_pred_4
+yy = np.abs(xx-Y)
+plt.scatter(xx, yy, s=1, alpha=0.01)
+plt.axis([-0.03,1.03,-0.03,1.03])
+plt.xlabel('fitted value')
+plt.ylabel('residuals')
+
+
+
 print("completed")
+
+
 
